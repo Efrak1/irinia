c0f6e9bb1f484b2486a4eb36c1aa1ca5a91e670b
Efrak1 <MicrosoftHuawei321@gmail.com>
2024-12-19 15:41:31 +0300

    Footer unfix

--- src/app/gallery/Portfolio.tsx
+++ src/app/gallery/Portfolio.tsx
@@ -1,6 +1,5 @@
 import { Footer } from '@/app/Footer/Footer';
 import { Logo } from '@/app/Head/Logo';
-import { Gallery } from '@/components/Gallery';
 import React from 'react'
 
 const Portfolio = () => {
@@ -9,7 +8,7 @@ const Portfolio = () => {
       <Logo />
       <section id='portfolio' className=''>
         <h2 className='text-[32px] flex justify-center items-center w-full h-[32px] mb-4'>Галерея</h2>
-        <Gallery />
+        
       </section>
       <Footer />
     </>
